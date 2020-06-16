2d67537a4ea1583c0c502fbd569f02b5d41311fd
AndresRedondo87 <66962177+AndresRedondo87@users.noreply.github.com>
2020-06-16 20:38:51 +0200

    hier sollte jetzt endlich Commited sein?!

---
+++ HalloJan/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloJan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Klasse 16 Jetzt bist du dran
+            Console.WriteLine("----Kleinprogramm Hallo Jan----\n");
+
+
+            Console.Write("Wie heiße ich nochmal?:");
+            String name; 
+            name = Console.ReadLine();
+            Console.WriteLine("\n Hallo Jan, ich bin {0}!\n", name);
+
+            Console.WriteLine("Drücken Sie eine Taste zum beenden\n\n");
+
+
+            ////FEHLER PROVOZIEREN - Klasse 18 (+17)
+            /// Aus der Fehlerliste kann man die Beschreibungen vom Fehler finden, auch links zu Info darüber und sonst kann man 
+            /// die rechte maus benutzen und das ganze kopieren und Jan kommentieren.
+            //Console.WriteLine(hier ist etwas nicht richtig);
+            //Console.WriteLine("hier ist etwas nicht richtig");
+
+            //Console.WriteLine("hier "+ "ist "+ "etwas "+ "nicht "+ "richtig.");
+            ////
+            ///
+
+            Console.ReadKey();
+        }
+    }
+}
